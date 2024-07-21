8bf7f41d405cd4f9efc441ec009dc0a8a63b13e4
stedreme <tedremets.sixten@gmail.com>
2024-07-21 16:43:48 +0300

    works?

--- got-the-time.js
+++ got-the-time.js
@@ -1,5 +1,28 @@
-function printPrettyDate (date) {
+/*function printPrettyDate (date) {
   return "Today is " + date.getDate(date) + date.getMonth(date) + date.getYear(date) + ", and the time is " + date.toLocaleTimeString(date)
 }
 
-console.log(printPrettyDate(new Date("1997-04-02T20:52:20.250Z")))
+console.log(printPrettyDate(new Date("1997-04-02T20:52:20.250Z")))*/ // DIDNT WORK
+
+function printPrettyDate (date) {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  
+  const day = days[date.getDay()];
+  const month = months[date.getMonth()];
+  const dayOfMonth = date.getDate();
+  const year = date.getYear();
+
+  let hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM'
+
+  hours = hours % 12 || 12; 
+
+  const formattedDate = `Today is ${day}, ${month} ${date}, ${year}, and the time is ${hours}:${minutes}:${seconds} ${ampm}`
+  console.log(formattedDate)
+}
+
+const date = new Date();
+printPrettyDate(date)
